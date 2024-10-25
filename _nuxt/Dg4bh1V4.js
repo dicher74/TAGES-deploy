5ceadd743f37c158ea6994d8968693d789e0305f
dicher74 <dicher74@gmail.com>
2024-10-25 20:29:13 +0300

    version-1.2

--- _nuxt/Dg4bh1V4.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,v as c,z as o,t as _,P as n}from"./CFYMVZOp.js";const a={},s={class:"content-wrapper"};function r(d,p){const e=n;return _(),c("div",s,[o(e)])}const l=t(a,[["render",r],["__scopeId","data-v-26bf0b60"]]);export{l as default};
