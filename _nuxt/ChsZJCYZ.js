3060a11ecb0f5c7ebfe267b48c864a4ce939a249
dicher74 <dicher74@gmail.com>
2024-10-25 20:03:51 +0300

    version-1

---
+++ _nuxt/ChsZJCYZ.js
@@ -0,0 +1,1 @@
+import{_ as t,v as c,z as o,t as _,P as n}from"./DNonIHVH.js";const a={},s={class:"content-wrapper"};function r(d,p){const e=n;return _(),c("div",s,[o(e)])}const l=t(a,[["render",r],["__scopeId","data-v-26bf0b60"]]);export{l as default};
